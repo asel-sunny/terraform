6d47261bd70b30054ba0b7467b5e42f0bd253f02
Asel Nazarova <aselnazarovaa@gmail.com>
2024-03-04 21:40:01 -0600

    still

--- web/alb.tf
+++ web/alb.tf
@@ -5,6 +5,10 @@ resource "aws_lb_target_group" "my_app_launch" {
   vpc_id      = aws_default_vpc.default.id
   target_type = var.target_type
   tags        = local.common_tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_lb" "my_app_launch" {
@@ -12,6 +16,7 @@ resource "aws_lb" "my_app_launch" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.sg-alb.id]
+  tags = local.common_tags
 
   subnets = [
     aws_default_subnet.pub-sub1.id,
@@ -30,6 +35,8 @@ resource "aws_lb" "my_app_launch" {
 #   }
 # }
 
+
+# creating a listener on port 443 with forward action
 resource "aws_lb_listener" "my_app_tls" {
   load_balancer_arn = aws_lb.my_app_launch.arn
   port              = "443"
@@ -44,11 +51,12 @@ resource "aws_lb_listener" "my_app_tls" {
   depends_on = [aws_acm_certificate_validation.acm_validation]
 }
 
+# creating a listener on port 80 with redirect action
 resource "aws_lb_listener" "my_app_http" {
   load_balancer_arn = aws_lb.my_app_launch.arn
   port              = "80"
   protocol          = "HTTP"
-  
+
     default_action {
       type = "redirect"
 
